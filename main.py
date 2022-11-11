6dd710426617459f77aec6c0b841bd7ad40aac43
velusya <117757905+velusya@users.noreply.github.com>
2022-11-11 18:30:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
-import codecs
 import time
 
-driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
+driver = webdriver.Edge(executable_path= r".\driver\msedgedriver.exe", options="")
 
 
 
 def main():
     try:
         driver.get("https://www.wildberries.ru/")
-        time.sleep(3)
+        time.sleep(5)
 
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        time.sleep(5)
 
         goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
 
@@ -21,15 +20,16 @@ def main():
 
         for good in goods_arr:
             good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
+            good_price = good.find_element(By.CSS_SELECTOR, '.goods-card__price-now').text
+            good_link = good.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+            goods_str = goods_str + good_title + ' ' + good_price + '\n' + good_link + '\n'
 
         
 
 
-        with codecs.open("parseRes.txt", "w", "utf-16") as stream:   # or utf-8
-            stream.write(goods_str)
-
+        file = open(r'C:\Users\79181\OneDrive\Рабочий стол\selenium intro\textgoodswb.txt', 'w', encoding= 'utf-16')
+        file.write(goods_str)
+        file.close
     except Exception as ex:
         print("An error occured: \n" + str(ex))
 
