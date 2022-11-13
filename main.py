5bdf616da9a62c2f5a176b66a5e9ad244fd544a9
iliadurmanov <mr.dia.2003@yandex.ru>
2022-11-13 11:09:00 +0300

    parsing from Durmanov

--- main.py
+++ main.py
@@ -12,17 +12,24 @@ def main():
         driver.get("https://www.wildberries.ru/")
         time.sleep(3)
 
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        # time.sleep(5)
+
+        y = 1000
+        for timer in range(0,7):
+            driver.execute_script("window.scrollTo(0, "+str(y)+")")
+            y += 1000  
+            time.sleep(1)
 
         goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
 
         goods_str = ''
 
         for good in goods_arr:
-            good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
+            if good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text != '':
+                good_info ='Good: ' + good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text + '\nLink: ' + good.find_element(By.CSS_SELECTOR, 'a').get_attribute('href') + '\nPrice: ' + good.find_element(By.CSS_SELECTOR, ".goods-card__price-now").text + '\n\n'
+                print(good_info)
+                goods_str = goods_str + good_info + "\n"
 
         
 
