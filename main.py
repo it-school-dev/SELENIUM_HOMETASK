c6cdd204627d8b87343195d8eb3c073b48592470
Yulia Byzeeva <yuliabyzeeva@MacBook-Air-Yulia.local>
2022-11-13 16:35:03 +0300

    its not working

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ import time
 driver = webdriver.Chrome(executable_path="./webdriver/chromedriver")
 
 
-
 def main():
     try:
         driver.get("https://www.wildberries.ru/")
-        time.sleep(3)
+        time.sleep(5)
 
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        time.sleep(5)
 
         goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
 
@@ -21,9 +20,10 @@ def main():
 
         for good in goods_arr:
             good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
-
+            good_price = good.find_element(By.CSS_SELECTOR, '.goods-card__price-now"').text
+            good_link = good.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+            goods_str = goods_str + good_title + ' ' + good_price + '\n' + good_link + '\n'
+        
 
 
         with codecs.open("parseRes.txt", "w", "utf-16") as stream:   # or utf-8
@@ -34,3 +34,4 @@ def main():
 
 
 main()
+
