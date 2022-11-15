7d7dd62c094aa450ec447abfa4c5bb2009d7fde0
VeronaPl <vp31102003@gmail.com>
2022-11-15 23:00:36 +0300

    parsing data to json and txt

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from selenium import webdriver 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 import codecs
 import time
+import json
 
-driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
-
+driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe", options="")
 
 
 def main():
@@ -12,26 +12,44 @@ def main():
         driver.get("https://www.wildberries.ru/")
         time.sleep(3)
 
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        y = 1000
+        for timer in range(0,10):
+            driver.execute_script("window.scrollTo(0, "+str(y)+")")
+            y += 1000
+            time.sleep(1)
 
         goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
-
         goods_str = ''
+        mas = []
 
         for good in goods_arr:
             good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
+            good_price = good.find_element(By.CLASS_NAME, "goods-card__price-now").text
+            good_url = good.find_element(By.CSS_SELECTOR, ".goods-card__container").get_attribute('href')
+            print(good_title, good_price, good_url)
+            goods_str = goods_str + good_title + "\t" + good_price + "\t" + str(good_url) + "\n"
+            mas.append([str(good_title), str(good_price), str(good_url)])
 
+        arr = {i + 1: {
+            'title': mas[i][0],
+            'price': mas[i][1],
+            'url': mas[i][2]
+        } for i in range(len(mas))}
         
-
-
         with codecs.open("parseRes.txt", "w", "utf-16") as stream:   # or utf-8
             stream.write(goods_str)
 
+        print(arr)
+        
+        with open('parsingData.json', 'w', encoding='utf-8') as f:
+            json.dump(arr, f, indent=4, sort_keys=False, ensure_ascii=False)
+
     except Exception as ex:
         print("An error occured: \n" + str(ex))
+
+    finally:
+        driver.close()
+        driver.quit()
 
 
 main()
