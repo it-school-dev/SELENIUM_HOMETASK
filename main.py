fcfc027f9709f4aa9660546691899d67ed46e2ab
Ivan Zhbanov <john190703@gmail.com>
2022-11-15 17:40:42 +0300

    output name, price and url without human

--- main.py
+++ main.py
@@ -1,37 +1,30 @@
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-import codecs
 import time
 
-driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
+driver = webdriver.Chrome(service=Service('.\webdriver\chromedriver.exe'))
+driver.maximize_window()
 
-
+#Сделать адекватную самостоятельную промотку
+#Перенести в файл(json). Возможно, объектами
 
 def main():
     try:
-        driver.get("https://www.wildberries.ru/")
-        time.sleep(3)
-
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        driver.get('https://www.wildberries.ru/')
         time.sleep(2)
 
-        goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight/2);')
+        time.sleep(2)
 
-        goods_str = ''
-
+        goods_arr=driver.find_elements(By.CLASS_NAME, 'goods__item')
         for good in goods_arr:
-            good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
-
-        
-
-
-        with codecs.open("parseRes.txt", "w", "utf-16") as stream:   # or utf-8
-            stream.write(goods_str)
-
+            good_title=good.find_elements(By.CSS_SELECTOR,'.goods-card__description span')[1].text
+            good_price=good.find_element(By.CLASS_NAME,'goods-card__price-now').text
+            good_url=good.find_element(By.CLASS_NAME, 'goods-card__container').get_attribute('href')
+            print(good_title,good_price,good_url)
+        print(len(goods_arr))
     except Exception as ex:
-        print("An error occured: \n" + str(ex))
-
+        print('Error with '+str(ex))
 
 main()
