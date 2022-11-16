c00d120f43a3712f0f763b2d9f4390727c499e85
ayneskkk <ksennny.ya@mail.ru>
2022-11-16 16:37:23 +0500

    домашка 2 ксения к

--- main.py
+++ main.py
@@ -10,19 +10,23 @@ driver = webdriver.Chrome(executable_path="./webdriver/chromedriver.exe")
 def main():
     try:
         driver.get("https://www.wildberries.ru/")
-        time.sleep(3)
+        time.sleep(5)
 
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(2)
+        time.sleep(5)
+
+        driver.execute_script('window.scrollTo({top: 0, behavior: "smooth"});')
+        time.sleep(4)
 
         goods_arr = driver.find_elements(By.CLASS_NAME, "goods__item")
 
         goods_str = ''
-
         for good in goods_arr:
             good_title = good.find_elements(By.CSS_SELECTOR, ".goods-card__description span")[1].text
-            print(good_title)
-            goods_str = goods_str + good_title + "\n"
+            good_link = good.find_element(By.CSS_SELECTOR, 'a').get_attribute('href')
+            good_price = good.find_element(By.CSS_SELECTOR, '.goods-card__price-now').text
+            goods_str = goods_str + good_title + ' ' + good_price + '\n' + good_link + '\n'
+            print(goods_str)
 
         
 
